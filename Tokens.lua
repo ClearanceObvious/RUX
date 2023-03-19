f2c233e53616182f11cf12540815f6d8c5b64863
ClearanceObvious <95777574+ClearanceObvious@users.noreply.github.com>
2023-03-19 19:13:25 +0100

    Update Tokens.lua

--- Tokens.lua
+++ Tokens.lua
@@ -6,12 +6,15 @@ tokens.MUL = 'MUL'
 tokens.DIV = 'DIV'
 
 tokens.EQ = 'EQUALS'
+tokens.COLON = 'COLON'
 
 tokens.EEQ = 'DOUBLEEQUALS'
 tokens.NEEQ = 'NOTDOUBLEEQUALS'
 
 tokens.LT = 'LESSTHAN'
 tokens.GT = 'GREATERTHAN'
+
+tokens.COMMA = 'COMMA'
 
 tokens.LTE = 'LESSTHANOREQUAL'
 tokens.GTE = 'GREATERTHANOREQUAL'
@@ -21,6 +24,7 @@ tokens.OR = 'IFSTATEMENTOR'
 
 tokens.STR = 'STRING'
 tokens.NUM = 'NUM'
+tokens.BOOL = 'BOOLEAN'
 
 tokens.LSTR = 'LEFTSTRING'
 tokens.RSTR = 'RIGHTSTRING'
