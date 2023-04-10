a3a0ea7c225c67309b4e58859358064101efa5a1
ClearanceObvious <95777574+ClearanceObvious@users.noreply.github.com>
2023-04-10 23:20:35 +0200

    Update Lexer.lua

--- Lexer.lua
+++ Lexer.lua
@@ -37,7 +37,7 @@ function strInTable(str, t: {})
 			return true
 		end
 	end
-	
+
 	return false
 end
 
@@ -46,7 +46,7 @@ function lexer.new(input)
 	self.text = input
 	self.currentNum = 1
 	self.currentChar = string.sub(self.text, self.currentNum, self.currentNum)
-	
+
 	return self
 end
 
@@ -57,11 +57,21 @@ end
 
 function lexer:lex()
 	local tokens = {}
-	
+
 	while self.currentChar ~= nil and self.currentNum <= string.len(self.text) do
 		if isWhiteSpace(self.currentChar) then
 			self:advance()
 			continue
+		elseif self.currentChar == '"' then
+			tokens[#tokens+1] = {type = tt.LSTR}
+			self:advance()
+			tokens[#tokens+1] = self:getStr()
+			if self.currentChar ~= '"' then
+				print(self.currentChar)
+				error('Expected "')
+			end
+			tokens[#tokens+1] = {type = tt.RSTR}
+			self:advance()
 		elseif self.currentChar == '!' then
 			if self.text:sub(self.currentNum+1, self.currentNum+1) == '=' then
 				tokens[#tokens+1] = {type = tt.NEEQ, value = nil}
@@ -109,11 +119,32 @@ function lexer:lex()
 				self:advance(); self:advance()
 				tokens[#tokens+1] = {type = tt.COMMENT, value = self:getComment()}
 				continue
+			elseif self.text:sub(self.currentNum+1, self.currentNum+1) == '=' then
+				self:advance(); self:advance()
+				tokens[#tokens+1] = {type = tt.CDIV, value = nil}; continue
 			end
 			tokens[#tokens+1] = {type = tt.DIV, value = nil}
 			self:advance()
 		elseif self.currentChar == '+' then
+			if self.text:sub(self.currentNum+1, self.currentNum+1) == '=' then
+				self:advance(); self:advance()
+				tokens[#tokens+1] = {type = tt.CPLUS, value = nil}; continue
+			end
 			tokens[#tokens+1] = {type = tt.PLUS, value = nil}
+			self:advance()
+		elseif self.currentChar == '-' then
+			if self.text:sub(self.currentNum+1, self.currentNum+1) == '=' then
+				self:advance(); self:advance()
+				tokens[#tokens+1] = {type = tt.CMIN, value = nil}; continue
+			end
+			tokens[#tokens+1] = {type = tt.MINUS, value = nil}
+			self:advance()
+		elseif self.currentChar == '*' then
+			if self.text:sub(self.currentNum+1, self.currentNum+1) == '=' then
+				self:advance(); self:advance()
+				tokens[#tokens+1] = {type = tt.CMUL, value = nil}; continue
+			end
+			tokens[#tokens+1] = {type = tt.MUL, value = nil}
 			self:advance()
 		elseif self.currentChar == '{' then
 			tokens[#tokens+1] = {type = tt.LCB, value = nil}
@@ -121,12 +152,11 @@ function lexer:lex()
 		elseif self.currentChar == '}' then
 			tokens[#tokens+1] = {type = tt.RCB, value = nil}
 			self:advance()
-		elseif self.currentChar == '-' then
-			tokens[#tokens+1] = {type = tt.MINUS, value = nil}
+		elseif self.currentChar == '[' then
+			tokens[#tokens+1] = {type = tt.LQP, value = nil}
 			self:advance()
-		elseif self.currentChar == '*' then
-			tokens[#tokens+1] = {type = tt.MUL, value = nil}
-			self:advance()
+		elseif self.currentChar == ']' then
+			tokens[#tokens+1] = {type = tt.RQP, value = nil}
 			self:advance()
 		elseif self.currentChar == ':' then
 			tokens[#tokens+1] = {type = tt.COLON, value = nil}
@@ -134,24 +164,14 @@ function lexer:lex()
 		elseif self.currentChar == ',' then
 			tokens[#tokens+1] = {type = tt.COMMA, value = nil}
 			self:advance()
-		elseif self.currentChar == '"' then
-			tokens[#tokens+1] = {type = tt.LSTR}
-			self:advance()
-			tokens[#tokens+1] = self:getStr()
-			if self.currentChar ~= '"' then
-				print(self.currentChar)
-				error('Expected "')
-			end
-			tokens[#tokens+1] = {type = tt.RSTR}
-			self:advance()
 		elseif self.currentChar == '(' then
 			tokens[#tokens+1] = {type = tt.LP, value = nil}
 			self:advance()
-		elseif self.currentChar == ';' then
-			tokens[#tokens+1] = {type = tt.EOL, value = nil}
-			self:advance()
 		elseif self.currentChar == ')' then
 			tokens[#tokens+1] = {type = tt.RP, value = nil}
+			self:advance()
+		elseif self.currentChar == ';' then
+			tokens[#tokens+1] = {type = tt.EOL, value = nil}
 			self:advance()
 		elseif self.currentChar == '$' then
 			tokens[#tokens+1] = {type = tt.AID, value = nil}
@@ -164,33 +184,33 @@ function lexer:lex()
 			error('Unknown Character ' .. self.currentChar ..  ' Occured')
 		end
 	end
-	
+
 	return tokens
 end
 
 function lexer:getStr()
 	local str = ''
-	
+
 	while charIn(self.currentChar, strtable) do
 		str = str .. self.currentChar
 		self:advance()
 	end
-	
+
 	return {type = tt.STR, value = str}
 end
 
 function lexer:getID()
 	local id = ''
-	
+
 	while charIn(self.currentChar, lowercase .. uppercase .. '_0123456789') do
 		id = id .. self.currentChar
 		self:advance()
 	end
-	
+
 	if strInTable(id, self.keywords) then
 		return {type = tt.KW, value = id}
 	end
-	
+
 	if id == 'true' then
 		return {type = tt.BOOL, value = true}
 	elseif id == 'false' then
@@ -207,14 +227,14 @@ function lexer:getComment()
 		str = str .. self.currentChar
 		self:advance()
 	end
-	
+
 	return str
 end
 
 function lexer:getNumber()
 	local dots = 0
 	local strNum = ''
-	
+
 	while charIn(self.currentChar, '0123456789.') do
 		if self.currentChar == '.' then
 			if dots >= 1 then
@@ -229,7 +249,7 @@ function lexer:getNumber()
 			self:advance()
 		end
 	end
-	
+
 	return tonumber(strNum)
 end
 
